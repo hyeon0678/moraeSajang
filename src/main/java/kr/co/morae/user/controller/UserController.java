4cf6710834341c21588b4fa433daea1b5bf2e70e
choichieon <codo7717@naver.com>
2023-11-09 18:49:21 +0900

    유저 수정

--- src/main/java/kr/co/morae/user/controller/UserController.java
+++ src/main/java/kr/co/morae/user/controller/UserController.java
@@ -98,4 +98,58 @@ public class UserController {
 		return page;
 	}
 	
+
+	@RequestMapping(value="/user/register")
+	public String mainregister() {
+		return "main/register";
+	}
+	
+
+	
+	@RequestMapping(value="/user/overlay")
+	@ResponseBody 
+	public HashMap<String, Object> overlay(@RequestParam String id) {
+		boolean use = service.overlay(id);
+		logger.info("사용 가능 여부: "+use);
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("use", use);	
+		logger.info("overlay");
+		return map;
+	}
+	
+	
+
+	@RequestMapping(value="/user/overemail")
+	@ResponseBody 
+	public HashMap<String, Object> overemail(@RequestParam String email) {
+		boolean use1 = service.overemail(email);
+		logger.info("사용 가능 여부 : "+use1);
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("use1", use1);		
+		return map;
+	}
+	
+	
+	
+	@RequestMapping(value="/user/overnickname")
+	@ResponseBody 
+	
+	public HashMap<String, Object> overnickname(@RequestParam String nickname) {
+		boolean use2 = service.overnickname(nickname);
+		logger.info("사용 가능 여부 : "+use2);
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("use2", use2);		
+		return map;
+	}
+	
+	@RequestMapping(value="/user/register", method = RequestMethod.POST)
+	@ResponseBody
+	public HashMap<String, Object> join(@RequestParam HashMap<String, String> params){
+		logger.info("params : "+params);
+		HashMap<String, Object> result = new HashMap<String, Object>();	
+		int row = service.join(params);		
+		result.put("success", row);		
+		return result;
+	}
+	
 }
